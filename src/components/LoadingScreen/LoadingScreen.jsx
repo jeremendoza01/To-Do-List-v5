3b5b0f2cbf05ba25b36b80cb02fe88480906e25c
jeremendoza01 <mjeremiasadriel@gmail.com>
2024-10-29 16:32:02 -0300

    commit loadingscreen

---
+++ src/components/LoadingScreen/LoadingScreen.jsx
@@ -0,0 +1,12 @@
+// LoadingScreen.js
+import React from 'react';
+import './LoadingScreen.css';
+
+const LoadingScreen = () => (
+    <div className="loading-screen">
+        <div className="spinner"></div>
+        <p>Loading...</p>
+    </div>
+);
+
+export default LoadingScreen;
